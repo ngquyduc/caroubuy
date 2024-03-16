50fa73eead8c665bacbc7b2eecf852d695c1f386
Quy Duc <quyduc711@gmail.com>
2024-03-17 01:40:54 +0800

    add puppeteer and scrapper through puppeteer

--- lib/browser.ts
+++ lib/browser.ts
@@ -1,41 +1,31 @@
-const puppeteer = require('puppeteer')
-const cheerio = require('cheerio')
+import { Browser, executablePath } from 'puppeteer'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-async function scrapeDescription(html) {
-  const browser = await puppeteer.launch()
+import scrapeCarousellProduct from './scapper'
+
+export const fetchHTML = async (url: string) => {
+  'use server'
+
+  puppeteer.use(StealthPlugin())
+  const browser: Browser = await puppeteer.launch({
+    headless: false,
+    executablePath: executablePath(),
+  })
   const page = await browser.newPage()
-
-  await page.setContent(html, { waitUntil: 'domcontentloaded' })
-
-  // Click the "Read more" button to reveal the full content
-  const readMoreButton = await page.$('button.D__r.D__H.D_acf.D_ach.D__z.D__v')
-  if (readMoreButton) {
-    await readMoreButton.click()
-    await page.waitForTimeout(500) // Wait for the content to update
-  }
-
-  // Extract the full description content
-  const fullDescriptionContent = await page.evaluate(() => {
-    const descriptionElement = document.querySelector(
-      'div.D_blH p.D_mH.D_mI.D_mM.D_mP.D_mS.D_mU.D_blH.D_blJ.D_nl'
-    )
-    return descriptionElement ? descriptionElement.textContent.trim() : ''
+  await page.goto(url)
+  // await page.waitForSelector('img', {
+  //   visible: true
+  // })
+  const data = await page.evaluate(async () => {
+    return {
+      html: document.documentElement.innerHTML,
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    }
   })
 
+  scrapeCarousellProduct(data.html)
+  await page.screenshot({ path: '1.png' })
   await browser.close()
-
-  return fullDescriptionContent
 }
-
-// Usage example
-const html = `
-  <!-- Your HTML content goes here -->
-`
-
-scrapeDescription(html)
-  .then((description) => {
-    console.log('Description:', description)
-  })
-  .catch((error) => {
-    console.error('Error:', error)
-  })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,16 @@ import { A } from '@solidjs/router'
 import Counter from '~/components/Counter'
 import scrapeCarousellProduct from '../../lib/scapper'
 import { createResource } from 'solid-js'
+import { fetchHTML } from '../../lib/browser'
 
 export default function Home() {
   const product_url =
     'https://www.carousell.sg/p/thinkpad-x220-1290079037/?t-id=fgHnHCjZR9_1710598330050&t-product_id=1290079037&t-referrer_browse_type=item_item_rec&t-referrer_page_type=listing_page&t-referrer_product_id=1291303404&t-referrer_request_id=8-CB9iOAWBRf1-BV&t-referrer_source=listing_page&t-tap_index=13'
   return (
-    <main>
+    <main class="flex justify-center items-center h-screen w-screen">
       <Button.Root
         onClick={async () => {
-          await scrapeCarousellProduct(product_url)
+          await fetchHTML(product_url)
         }}
       >
         Click me
