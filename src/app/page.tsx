4f785cea062569ce005d9400e7adf7c78aeb0819
Quy Duc <quyduc711@gmail.com>
2024-03-23 23:24:28 +0800

    set up next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import SearchBar from '@/components/SearchBar'
-import { prisma } from '@/prisma'
 
 export default function Home() {
-  prisma.products.findMany().then((product) => {
-    console.log(product)
-  })
   return (
-    <main className='flex h-screen w-screen justify-center items-center space-x-2'>
+    <main className='flex flex-col h-screen w-screen justify-center items-center space-x-2'>
       <SearchBar />
     </main>
   )
