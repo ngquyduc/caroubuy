071528b58dda0c798a2411e02e5485a8923af4ee
Nguyen Quy Duc <quyduc711@gmail.com>
2024-06-07 14:17:23 +0800

    further clean up

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,6 @@ const nextConfig = {
     esmExternals: "loose", // <-- add this
     serverComponentsExternalPackages: ["mongoose"], // <-- and this
   },
-  // and the following to enable top-level await support for Webpack
-  // webpack: (config) => {
-  //   config.experiments = {
-  //     topLevelAwait: true,
-  //   };
-  //   return config;
-  // },
 };
 
 export default nextConfig;
